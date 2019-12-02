462f7d98421bc279fadbea830599c0c1de5c92b0
andersmoldin <anders@moldin.com>
2019-12-02 08:18:13 +0100

    First part of first day

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _01
 {
@@ -6,7 +9,26 @@ namespace _01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadLines("input.txt");
+
+            Console.WriteLine($"Part 1: {Part1(input)}");
+            Console.WriteLine($"Part 2: {Part2(input)}");
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2019/day/1
+        /// </summary>
+        private static int Part1(IEnumerable<string> masses)
+        {
+            return masses.Select(mass => ((int)Math.Floor(double.Parse(mass) / 3)) - 2).Sum();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2019/day/1#part2
+        /// </summary>
+        private static int Part2(IEnumerable<string> changes)
+        {
+            throw new NotImplementedException();
         }
     }
 }
