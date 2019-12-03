22583789d4300bb09c136180b838a00725a6676c
andersmoldin <anders@moldin.com>
2019-12-03 10:29:45 +0100

    Day 1 completed

--- 01/Program.cs
+++ 01/Program.cs
@@ -9,7 +9,7 @@ namespace _01
     {
         static void Main(string[] args)
         {
-            var input = File.ReadLines("input.txt");
+            var input = File.ReadLines("input.txt").Select(int.Parse).ToList();
 
             Console.WriteLine($"Part 1: {Part1(input)}");
             Console.WriteLine($"Part 2: {Part2(input)}");
@@ -18,17 +18,32 @@ namespace _01
         /// <summary>
         /// https://adventofcode.com/2019/day/1
         /// </summary>
-        private static int Part1(IEnumerable<string> masses)
+        private static int Part1(IEnumerable<int> masses)
         {
-            return masses.Select(mass => ((int)Math.Floor(double.Parse(mass) / 3)) - 2).Sum();
+            return masses.Select(mass => GetFuelForMass(mass)).Sum();
         }
 
         /// <summary>
         /// https://adventofcode.com/2019/day/1#part2
         /// </summary>
-        private static int Part2(IEnumerable<string> changes)
+        private static int Part2(IEnumerable<int> masses)
         {
-            throw new NotImplementedException();
+            return masses.ToList().Select(mass => GetFuelForFuelAndMass(mass)).Sum();
+        }
+
+        private static int GetFuelForMass(int mass)
+        {
+            return (int)(Math.Floor((double)mass / 3)) - 2;
+        }
+
+        private static int GetFuelForFuelAndMass(int mass)
+        {
+            var sum = (int)(Math.Floor((double)mass / 3)) - 2;
+
+            if (sum <= 0)
+                return 0;
+
+            return sum + GetFuelForFuelAndMass(sum);
         }
     }
 }
